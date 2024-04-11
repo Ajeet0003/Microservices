8c67d31d72920750c228cf9ada5e69f53dbeb5c5
Ajeet Kumar <92078263+Ajeet0003@users.noreply.github.com>
2024-04-11 22:01:11 +0530

    Delete Microservices directory

--- Microservices/HotelService/src/main/java/com/rating/service/repositories/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rating.service.repositories;
-
-import com.rating.service.entiries.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepository extends JpaRepository<Hotel, String> {
-}
--- Microservices/UserService/src/main/java/com/user/service/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.user.service.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.user.service.entiries.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
